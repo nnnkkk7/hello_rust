babf235526d324c638049bf15c743ed93104015b
nnnkkk7 <kurodanaoki0711pana@gmail.com>
2021-02-13 14:48:33 +0900

    hello world

---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+/Users/kurodanaoki/rust/hello_rust/hello_cargo/target/debug/deps/hello_cargo: src/main.rs
+
+/Users/kurodanaoki/rust/hello_rust/hello_cargo/target/debug/deps/hello_cargo.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/kurodanaoki/rust/hello_rust/hello_cargo/target/debug/hello_cargo: /Users/kurodanaoki/rust/hello_rust/hello_cargo/src/main.rs
